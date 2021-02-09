49f306f737512050dad4477972dcf29bcdc56c99
Kyuwoong Lee <kwooler@gmail.com>
2021-02-09 18:49:20 +0000

    Q

---
+++ Assignment 1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomap>
+using namespace std;
+
+int main();
+{
+  const int rateA = 15;
+  const int rateB = 12;
+  const int rateC = 9;
+
+  int seatA, seatB, seatC;
+  
+
+}
+
