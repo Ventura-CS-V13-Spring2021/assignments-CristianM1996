e7d462971a974ee46235fc16a9476f5b6d57fd4d
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-16 22:07:52 +0000

    2-6 not finished

---
+++ Assignment2-6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+  int randN;
+  int N;
+  ofstream readf;
+  readf.open("randN.txt");
+
+  cout << "Enter the amount of random number: ";
+  cin >> N;
+  readf << N << endl;
+  srand(time(0));
+  for(int i; i < N; i++)
+  {
+    randN = random() % 100;
+    readf << randN << endl;
+  }
+  readf.close();
+
+    int sum=0;
+    ifstream rdfile;
+    rdfile.open("randN.txt");
+
+    while(rdfile >> randN)
+    {
+      sum += randN;
+      cout << randN << endl;
+    }
+
+    cout << "Sum " << sum << endl;
+    rdfile.close();
+  
+    int avg;
+    avg = sum / (N + 1);
+    cout << "Average " << avg;
+    
+}
