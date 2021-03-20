1d13b7f99e0e6923b809bf29e681968f5011879b
Kyuwoong Lee <kwooler@gmail.com>
2021-03-20 19:30:17 +0000

    Q

--- Assignment2-6.cpp
+++ Assignment2-6.cpp
@@ -6,37 +6,44 @@ using namespace std;
 
 int main()
 {
-  int randN;
-  int N;
-  ofstream readf;
-  readf.open("randN.txt");
+	int randN;
+	int N;
+	ofstream readf;
+	readf.open("randN.txt");
 
-  cout << "Enter the amount of random number: ";
-  cin >> N;
-  readf << N << endl;
-  srand(time(0));
-  for(int i; i < N; i++)
-  {
-    randN = random() % 100;
-    readf << randN << endl;
-  }
-  readf.close();
+	cout << "Enter the amount of random number: ";
+	cin >> N;
+	readf << N << endl;
+	srand(time(0));
+	for(int i; i < N; i++)
+	{
+	randN = random() % 100;
+	readf << randN << endl;
+	}
+	readf.close();
 
-    int sum=0;
-    ifstream rdfile;
-    rdfile.open("randN.txt");
+	int sum=0;
+	ifstream rdfile;
+	rdfile.open("randN.txt");
 
-    while(rdfile >> randN)
-    {
-      sum += randN;
-      cout << randN << endl;
-    }
+	// (1) 
+	// Before the loop, MIN and MAX must be initialized as 100 and 0
+	// or initialized ad the first random number.
+	while(rdfile >> randN)
+	{
+		sum += randN;
+		cout << randN << endl;
 
-    cout << "Sum: " << sum << endl;
-    rdfile.close();
-  
-    double avg;
-    avg = sum / (N + 1.0);
-    cout << "Average:  " << avg << "\n";
-    cout << "Total number of integers: " << N + 1;
+		// (2) Here you can add the code to find MIN and MAX
+		// You can use the same code that we already used in other examples
+		
+	}
+
+	cout << "Sum: " << sum << endl;
+	rdfile.close();
+
+	double avg;
+	avg = sum / (N + 1.0);
+	cout << "Average:  " << avg << "\n";
+	cout << "Total number of integers: " << N + 1;
 }
