ffbdaf9207d5918abe1a25810a6d8c338024dc19
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-22 04:37:37 +0000

    Q2 done

--- Q2.cpp
+++ Q2.cpp
@@ -7,16 +7,17 @@ int main()
   cout << "Enter three integer values between 0 and 99: " ;
   cin >> num1 >> num2 >> num3;
 
-  if(num1 == num2)
-    cout << "There are two integers that are equal: " << num1 << " and " << num2 << endl;
-  else if(num2 == num3)
-      cout << "There are two integers that are equal: " << num2 << " and " << num3 << endl;
-  else if(num1 == num3)
-        cout << "There are two integers that are equal: " << num1 << " and " << num3 << endl;
-  else if (num1 != num2 && num2 != num3 && num1 != num3)  
-            cout << "Ther are no duplicate numbers." << endl;    
-  else if(num1 == num2 && num2 == num3)
-          cout << "All integers are equal." << num1 << ", " << ", " << num2 << " and " << num3 << endl;
+  if (num1 == num2 || num2 == num3 || num1 == num3)
+    if (num1 == num2)
+      cout << "There are two integers that are duplicate: " << num1 << " and " << num2 << endl;
+    else if (num2 == num3)
+      cout << "There are two integers that are duplicate: " << num2 << " and " << num3 << endl;
+    else if (num1 == num3)
+        cout << "There are two integers that are duplicate: " << num1 << " and " << num3 << endl;
+      if (num1 != num2 && num2 != num3 && num1 != num3)  
+        cout << "There are no duplicate numbers." << endl;    
+      else if (num1 == num2 && num2 == num3)
+        cout << "All integers are duplicate: " << num1 << ", " << num2 << " and " << num3 << endl;
   
   return 0;
 
