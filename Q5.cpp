59a57468baec39cfa460d2b9e9923315712543f2
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-22 05:28:46 +0000

    check

--- Q5.cpp
+++ Q5.cpp
@@ -6,19 +6,19 @@ int main()
   int i, j, x, y;
   
     cout << "First integer must be less than the second.\n";
-    cout << "Enter the first integer for your range: ";
+    cout << "\nEnter the first integer for your range: ";
     cin >> x;
     cout << "Enter the second integer for your range: ";
     cin >> y;
     
-      for(i = x; i <= y; i++)
+    for(i = x; i <= y; i++)
+    {
+      for(j = 2; j <= i; j++)
       {
-        for(j = 2; j <= i; j++)
-        {
-          if(i % j == 0)
-          break;
+        if(i % j == 0)
+        break;
       }
-      if (j == i)
-        cout << i << endl;
-      }
+    if (j == i)
+      cout << i << endl;
+    }
 }
