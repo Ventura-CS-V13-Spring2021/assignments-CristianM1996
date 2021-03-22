5b2bb0c76ec7fa36fdd825c225232b6c83250eb3
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-22 05:02:08 +0000

    check

---
+++ Q5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i, j, x, y;
+  cout << "First integer must be less than the second.\n";
+  cout << "Enter the first integer for your range: ";
+  cin >> x;
+  cout << "Enter the second integer for your range: ";
+  cin >> y;
+
+  
+
+}
