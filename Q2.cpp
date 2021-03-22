26248fd1171fb713170954b6db643fc4836bb162
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-22 04:10:17 +0000

    check

--- Q2.cpp
+++ Q2.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
-int number1, number2, number3;
+  int num1, num2, num3;
+  cout << "Enter three integer values between 0 and 99: " ;
+  cin >> num1 >> num2 >> num3 ;
 
+  if(num1 == num2 || num2 == num3 || num1 == num3)
+    cout << "There are two integers that are equal.";
 }
