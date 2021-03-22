eaaf440b26f2495bc173a7b971d8d10fc7e9bd80
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-22 03:14:36 +0000

    q1

---
+++ Q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  srand(time(0));
+	int randN;
+  for(int i = 0; i < 5; i++)
+	{
+	  randN = random() % 99;
+	  cout << randN << endl;
+	}
+}
