c4feef8114348cbb6cc031657a264adda6de21fe
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-22 04:18:31 +0000

    check

--- Q2.cpp
+++ Q2.cpp
@@ -7,10 +7,15 @@ int main()
   cout << "Enter three integer values between 0 and 99: " ;
   cin >> num1 >> num2 >> num3 ;
 
-  if(num1 == num2 || num2 == num3 || num1 == num3)
-    cout << "There are two integers that are equal.";
+  if(num1 == num2)
+    cout << "There are two integers that are equal." << num1 << "and" << num2 << endl;
+  if(num2 == num3)
+    cout << "There are two integers that are equal." << num2 << "and" << num3 << endl;
+  if(num1 == num3)
+    cout << "There are two integers that are equal." << num1 << "and" << num3 << endl;
   else if(num1 == num2 && num2 == num3)
-    cout << "All integers are equal." << num1 << num2 << num3;
+    cout << "All integers are equal." << num1 << num2 << num3 << endl;
   
+  return 0;
 
 }
