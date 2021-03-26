1a68aa8c1a8f39837dbcefd7a073ab11c13232c1
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-26 08:44:10 +0000

    check

--- quiz2/Q7-part2.cpp
+++ quiz2/Q7-part2.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main()
 {
   ifstream rdfile;
-  int empID,numEmp;
+  int empID, numEmp;
   string empN, deptN;
   double sal, avg, sum;
   rdfile.open("employee.txt");
+  sum = 0;
+
+  while(rdfile >> empID >> sal >> deptN >> empN)
+  {
+    
+  }
 
   
