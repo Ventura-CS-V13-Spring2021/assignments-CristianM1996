5548cbac506e6d81af6374c6c86b78ccab9001c4
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-26 06:38:27 +0000

    check

--- quiz2/Q3.cpp
+++ quiz2/Q3.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main()
 {
   int num1, num2, num3;
-  cout << "Enter three integer values between 0 and 99: " ;
+  
+  do{
+  cout << "Enter three integer values from 0 to 99: " ;
   cin >> num1 >> num2 >> num3;
+  }while((num1 < 0 || num1 > 100) && (num))
 
   if (num1 == num2 || num2 == num3 || num1 == num3)
     if (num1 == num2)
