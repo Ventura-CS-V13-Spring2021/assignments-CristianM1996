3e2135f9882caf0f7157e3e2d2088dacb86d15af
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-26 05:55:00 +0000

    check

--- quiz2/Q1.cpp
+++ quiz2/Q1.cpp
@@ -6,10 +6,13 @@ int main()
 {
   srand(time(0));
 	int randN;
+  int num;
   for(int i = 0; i < 5; i++)
 	{
 	  randN = random() % 99;
-    //cout << randN << endl;
+    cout << randN << endl;
   }
-
+  num = random() % 99;
+  if(num > randN)
+  cout << num << ", " << randN << endl;
 }
