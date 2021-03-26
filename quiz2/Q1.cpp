fd3b5876d27161106cb2364174cc64a93a54c484
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-26 07:55:48 +0000

    check

--- quiz2/Q1.cpp
+++ quiz2/Q1.cpp
@@ -18,4 +18,4 @@ int main()
   }
 }
 
-//the for loop has two variables that are assigned random numbers and they go through the loop 3 times. The numbers assigned to randN are printed out. An if statement checks if the value assigned to num is greater than the value assigned to randN in each loop. If it's true, the print statement stating when the current number is greater than the previous number is outputted. I tried getting it to generate only 5 numbers but got stuck and the number of values outputted are inconsistant. 
+//Q2: The for loop has two variables that are assigned random numbers and they go through the loop 3 times. The numbers assigned to randN are printed out. An if statement checks if the value assigned to num is greater than the value assigned to randN in each loop. If it's true, the print statement stating when the current number is greater than the previous number is outputted. I tried getting it to generate only 5 numbers but got stuck and the number of values outputted are inconsistant. 
