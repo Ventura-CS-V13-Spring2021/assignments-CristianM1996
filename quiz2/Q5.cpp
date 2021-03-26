8b2d190434c36de9a27d78a00f80b791034d8ca8
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-26 07:31:50 +0000

    check

--- quiz2/Q5.cpp
+++ quiz2/Q5.cpp
@@ -4,21 +4,22 @@ using namespace std;
 int main()
 {
   int i, j, x, y;
-  
-    cout << "First integer must be less than the second.\n";
-    cout << "\nEnter the first integer for your range: ";
-    cin >> x;
-    cout << "Enter the second integer for your range: ";
-    cin >> y;
+  do{
+  cout << "First integer must be less than the second.\n";
+  cout << "Enter the first integer for your range: ";
+  cin >> x;
+  cout << "Enter the second integer for your range: ";
+  cin >> y;
+  }while(x > y);
     
-    for(i = x; i < y; i++)
+  for(i = x; i < y; i++)
+  {
+    for(j = 2; j < i; j++)
     {
-      for(j = 2; j < i; j++)
-      {
-        if(i % j == 0)
-        break;
-      }
-    if (j == i)
-      cout << i << endl;
+      if(i % j == 0)
+      break;
     }
+  if (j == i)
+    cout << i << endl;
+  }
 }
