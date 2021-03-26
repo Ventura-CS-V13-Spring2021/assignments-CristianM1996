6625b6c65d1f2f7ff27c26ecebeaf51917c8b249
Cristian Meza <cristian_meza1@my.vcccd.edu>
2021-03-26 08:10:01 +0000

    check

--- quiz2/Q7.cpp
+++ quiz2/Q7.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main()
 {
-  int empID;
+
+  int empID,numEmp;
   string empN, deptN;
   double sal;
   ofstream readf;
   readf.open("employee.txt")
+
+  cout << "Number of employees: " << endl;
+  cin >> numEmp;
+  
+  for(int i = 0; i < numEmp; i++)
+  {
+    
+  }
 }
