adafe251a95a626922b4ac7d18706012b8409fa9
Cristian Meza <>
2021-04-20 04:50:13 +0000

    check

--- Midterm/Q2.cpp
+++ Midterm/Q2.cpp
@@ -4,27 +4,33 @@ using namespace std;
 int isGreater(int n);
 int getRandom(void);
 int main(){
-  //ofstream readf;
-  //readf.open("numbers.txt");
-  
+  int num;
+  ofstream readf;
+  readf.open("numbers.txt");
+  readf << num << endl;
 }
 int getRandom(void){
   
   int randN, num;
-  ofstream readf;
-  readf.open("numbers.txt");
+  
   srand(time(0));
   do{
     randN = rand() % 50;
-    readf << randN << endl;
-    num = isGreater(n);
+   
+    num = isGreater(randN);
+    cout << num << endl;
   }while(num == 1);
 } 
 int isGreater(int n){
-  int prec
+  int prec = 0;
 
   if(prec < n){
     prec = n;
-    
+    return 1;
+  }
+  else
+  {
+    prec = n;
+    return 0;
   }
 }
